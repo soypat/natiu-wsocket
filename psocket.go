e845146787d8b165a39e692cce24532546406751
Patricio Whittingslow <pato@stamm.bio>
2023-01-17 14:14:47 -0300

    changes ongoing

--- psocket.go
+++ psocket.go
@@ -4,24 +4,28 @@ import (
 	"context"
 	"errors"
 
+	mqtt "github.com/soypat/natiu-mqtt"
 	"github.com/soypat/peasocket"
 )
 
 // PClient uses peasocket implementation.
 type PClient struct {
-	client *peasocket.Client
+	ws *peasocket.Client
+	mq *mqtt.Client
 }
 
 type PClientConfig struct {
 	ServerURL       string
 	WebsocketBuffer []byte
 	Entropy         func() uint32
+	MQTTDecoder     mqtt.Decoder
 }
 
 func NewPClient(cfg PClientConfig) *PClient {
 	ps := peasocket.NewClient(cfg.ServerURL, cfg.WebsocketBuffer, cfg.Entropy)
 	pc := &PClient{
-		client: ps,
+		ws: ps,
+		mq: mqtt.NewClient(cfg.MQTTDecoder),
 	}
 	return pc
 }
@@ -30,11 +34,41 @@ func (pc *PClient) Connect(ctx context.Context) error {
 	if pc.IsConnected() {
 		return errors.New("already connected")
 	}
-	err := pc.client.DialViaHTTPClient(ctx, nil)
+	err := pc.ws.DialViaHTTPClient(ctx, nil)
 	if err != nil {
 		return err
 	}
+	// pc.mq.SetTransport(pc.ws.)
 	return nil
 }
 
-func (pc *PClient) IsConnected() bool { return pc.client.Err() == nil }
+func (pc *PClient) IsConnected() bool { return pc.ws.Err() == nil }
+
+func (pc *PClient) callbacks() (mqtt.RxCallbacks, mqtt.TxCallbacks) {
+	return mqtt.RxCallbacks{
+			OnRxError: func(r *mqtt.Rx, err error) {
+				pc.closeConn(err)
+			},
+			OnConnack: func(r *mqtt.Rx, vc mqtt.VariablesConnack) error {
+				if vc.ReturnCode != 0 {
+					return errors.New(vc.ReturnCode.String())
+				}
+				return nil
+			},
+		}, mqtt.TxCallbacks{
+			OnTxError: func(tx *mqtt.Tx, err error) {
+				pc.closeConn(err)
+			},
+		}
+}
+
+func (pc *PClient) closeConn(err error) {
+
+}
+func (c *PClient) Ping(ctx context.Context) error {
+	if !c.IsConnected() {
+		return ErrNotConnected
+	}
+
+	return c.mq.Ping()
+}
